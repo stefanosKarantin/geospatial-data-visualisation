1570424bf3e94d88a8b80b97db46c1d63163c55a
stefanosKarantin <karantinakis.s@gmal.com>
2019-10-13 22:36:09 +0300

    serving static files with flask

--- server/server.py
+++ server/server.py
@@ -2,12 +2,10 @@ from flask import Flask
 from flask import render_template
 
 # creates a Flask application, named app
-app = Flask(__name__)
-
-# a route where we will display a welcome message via an HTML template
+app = Flask(__name__, static_folder="build/static", template_folder="build")
 @app.route("/")
 def hello():
- return 'Hello to the World of Flask!'
+    return render_template('index.html')
 
 # run the application
 if __name__ == "__main__":
