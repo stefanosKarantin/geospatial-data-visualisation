2f005df058e96fa2dc29b61b5d5e6d6df97765a9
stefanosKarantin <karantinakis.s@gmal.com>
2020-03-31 21:45:32 +0300

    adding datasets in db

--- postgres/db-init/gis-schema.sql
+++ postgres/db-init/gis-schema.sql
@@ -1,5 +1,74 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
 
-CREATE SCHEMA gisdata;
+CREATE TABLE countries (
+    id varchar(10),
+    name varchar(100),
+    geom geometry(polygon, 4326) not null,
+    primary key(id)
+);
 
+CREATE INDEX idx_countries ON countries USING gist(geom);
+
+-- BEGIN;
+-- CREATE TEMPORARY TABLE temp_json (values TEXT) ON COMMIT DROP;
+-- CREATE TEMPORARY TABLE temp_countries (
+--     id varchar(10),
+--     name varchar(100),
+--     geom geometry(polygon, 4326) not null
+-- ) ON COMMIT DROP;
+
+-- \copy temp_json FROM './countries.geojson';
+
+-- insert into temp_countries (id, name, geom)
+--   SELECT
+--     feat->>'id' AS id,
+--     feat->'properties'->>'name' AS name,
+--     ST_SetSRID(ST_AsText(ST_GeomFromGeoJSON(feat->>'geometry')), 4326) AS geom
+--   FROM (
+--     SELECT json_array_elements(fc->'features') AS feat
+--     FROM (
+--       SELECT values::json AS fc FROM temp_json
+--     ) AS t
+--   ) AS f;
+
+-- insert into countries (id, name, geom)
+-- select * from temp_countries order by name;
+
+-- COMMIT;
+
+CREATE TABLE regions (
+  id VARCHAR(200),
+  name VARCHAR(100),
+  geom geometry(multipolygon, 4326),
+  PRIMARY KEY (id)
+) ;
+
+CREATE INDEX idx_regions ON regions USING gist(geom);
+
+BEGIN;
+CREATE TEMPORARY TABLE temp_json (values TEXT) ON COMMIT DROP;
+CREATE TEMPORARY TABLE temp_regions (
+    id VARCHAR(200),
+    name VARCHAR(100),
+    geom geometry(multipolygon, 4326),
+    PRIMARY KEY (id)
+) ON COMMIT DROP;
+
+\copy temp_json FROM './periferies.geojson';
+
+insert into temp_regions (id, name, geom)
+  SELECT
+    feat->'properties'->>'PER' AS name,
+    feat->>'id' AS id,
+    ST_SetSRID(ST_AsText(ST_GeomFromGeoJSON(feat->>'geometry')), 4326) AS geom
+  FROM (
+    SELECT json_array_elements(fc->'features') AS feat
+    FROM (
+      SELECT values::json AS fc FROM temp_json
+    ) AS t
+  ) AS f;
+
+insert into regions (id, name, geom)
+select * from temp_regions order by name;
+COMMIT;
 
--- run-app.sh
+++ run-app.sh
@@ -9,3 +9,80 @@
 
 docker-compose up --build
 
+# #!/bin/bash
+
+# set -e
+# # function cleanup {
+# #   cd $cwd
+# # }
+
+# function show_help {
+# cat << EOF
+# -n skip build
+# -s activate stream
+# EOF
+# }
+
+# # trap cleanup EXIT
+
+# export PROJECT_DIR=$(pwd)
+# export PROJECT_BIN=$(pwd)/bin
+
+# cwd=$(pwd)
+# nobuild=0
+# # stream=0
+# webapp=0
+# while getopts "h?nw" opt; do
+#     case "$opt" in
+#     h|\?)
+#         show_help
+#         exit 0
+#         ;;
+#     n)  nobuild=1
+#     ;;
+#     # s)  stream=1
+#     # ;;
+#     w)  webapp=1
+#     esac
+# done
+
+# shift $((OPTIND - 1))
+
+# echo ""
+# echo "*****************************"
+# echo "No build      : $nobuild"
+# # echo "Stream source : $stream"
+# echo "Webapp Compile : $webapp"
+# echo "*****************************"
+# echo ""
+
+# # . $PROJECT_BIN/load_config_variables
+# #
+# # $PROJECT_BIN/print_config_variables
+
+# # Building webapp
+# if [ "$webapp" -eq "1" ]; then
+#   echo "running ---> npm run build ----> compiling frontend"
+#   cd ./webapp
+#   npm i
+#   npm run build
+#   cd ..
+# fi
+
+# if [ "$nobuild" -eq "0" ]; then
+#   # if [ "$stream" -eq "0" ]; then
+#     echo "running ---> docker-compose -f docker-compose.yml up --build"
+#     docker-compose -f docker-compose.yml up --build
+#   # else
+#   #   echo "running ---> docker-compose -f  docker-compose.yml up --build ---> with stream"
+#   #   docker-compose -f docker-compose.yml -f docker-compose.override.yml up --build
+#   # fi
+# else
+#   # if [ "$stream" -eq "0" ]; then
+#     echo "running ---> docker-compose -f docker-compose.yml up"
+#     docker-compose -f docker-compose.yml up
+#   # else
+#   #   echo "running ---> docker-compose -f docker-compose.yml up  ---> with stream"
+#   #   docker-compose -f docker-compose.yml -f docker-compose.override.yml up
+#   # fi
+# fi
